bf575ffc8e632c38bd2662ac1f47ea2a40d6ccb1
仲優樹 <yu-ki216m@outlook.jp>
2024-12-27 19:19:39 +0900

    Refactoring : Replaced ShowSelectedDateEmptyErrorText with ErrorText in ToeicRecordScreen.

--- app/src/main/java/com/example/proenglishscoretracker/record_screen/ToeicRecordScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/record_screen/ToeicRecordScreen.kt
@@ -80,7 +80,7 @@ fun ToeicRecordScreen(viewModel: EnglishInfoViewModel) {
                     selectedDateEmptyErrorText = ""
                 }
                 Text(selectedDate)
-                if (selectedDate.isEmpty()) ShowSelectedDateEmptyErrorText(selectedDateEmptyErrorText)
+                if (selectedDate.isEmpty()) ErrorText(selectedDateEmptyErrorText)
             }
         }
 
@@ -262,15 +262,6 @@ private fun SelectDatePicker(context: Context, onDateSelected: (String) -> Unit)
             color = Color.White,
         )
     }
-}
-
-@Composable
-private fun ShowSelectedDateEmptyErrorText(error: String) {
-    Text(
-        text = error,
-        fontSize = 12.sp,
-        color = Color.Red
-    )
 }
 
 @Composable
