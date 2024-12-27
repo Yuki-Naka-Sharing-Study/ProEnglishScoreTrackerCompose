3b08eafe862572e16444157ff63b5bf06da47a55
仲優樹 <yu-ki216m@outlook.jp>
2024-12-27 11:31:06 +0900

    Add : Add the variable “ErrorText” to the top in ToeflIbtRecordScreen.

--- app/src/main/java/com/example/proenglishscoretracker/record_screen/ToeflIbtRecordScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/record_screen/ToeflIbtRecordScreen.kt
@@ -60,7 +60,12 @@ fun ToeflIbtRecordScreen(viewModel: EnglishInfoViewModel) {
         var memoText by rememberSaveable { mutableStateOf("") }
 
         //「ErrorText」系
-
+        var selectedDateEmptyErrorText by remember { mutableStateOf("") }
+        var overallMaxScoreErrorText by remember { mutableStateOf("") }
+        var readingMaxScoreErrorText by remember { mutableStateOf("") }
+        var listeningMaxScoreErrorText by remember { mutableStateOf("") }
+        var writingMaxScoreErrorText by remember { mutableStateOf("") }
+        var speakingMaxScoreErrorText by remember { mutableStateOf("") }
 
         //「Error」系
         val selectedDateEmptyError = selectedDate.isEmpty()
