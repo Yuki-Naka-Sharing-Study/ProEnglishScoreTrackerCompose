ec75db8b7ec3ee373ff22cc73a870872d5cad69f
仲優樹 <yu-ki216m@outlook.jp>
2024-12-28 20:32:02 +0900

    Fix : Align the error message with the left edge of the ReadingImageView in ToeflIbtRecordScreen.

--- app/src/main/java/com/example/proenglishscoretracker/record_screen/ToeflIbtRecordScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/record_screen/ToeflIbtRecordScreen.kt
@@ -135,7 +135,13 @@ fun ToeflIbtRecordScreen(viewModel: EnglishInfoViewModel) {
                     value = readingScore,
                     onValueChange = { readingScore = it }
                 )
-                if (readingScore >= 31) ErrorText("Readingスコアは31未満である必要があります。")
+            }
+        }
+
+        Row {
+            Spacer(modifier = Modifier.padding(start = dimensionResource(id = R.dimen.space_52_dp)))
+            if (readingScore >= 31) {
+                ErrorText("Readingスコアは31未満である必要があります。")
             }
         }
 
