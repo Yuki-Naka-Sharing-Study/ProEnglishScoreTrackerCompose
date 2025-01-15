ac7330717c436900375cc8663703276918f77f14
仲優樹 <yu-ki216m@outlook.jp>
2025-01-15 21:39:16 +0900

    Implement logic to display an AlertDialog when a screen transition is attempted by TabRow without tapping the Register button after entering data in ExamRecordScreen.

--- app/src/main/java/com/example/proenglishscoretracker/bottom_navigation_and_tab_row/ExamRecordScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/bottom_navigation_and_tab_row/ExamRecordScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Color
 import com.example.proenglishscoretracker.data.EnglishInfoViewModel
 import com.example.proenglishscoretracker.record_screen.EikenRecordScreen
@@ -16,6 +18,9 @@ import com.example.proenglishscoretracker.record_screen.ToeicSwRecordScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.pagerTabIndicatorOffset
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterialApi::class, ExperimentalPagerApi::class)
 @Composable
@@ -28,9 +33,31 @@ fun ExamRecordScreen(viewModel: EnglishInfoViewModel) {
         TabItem.TOEIC,
     )
     val pagerState = com.google.accompanist.pager.rememberPagerState(tabs.size)
+    val scope = rememberCoroutineScope()
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+    var pendingPage by rememberSaveable { mutableIntStateOf(0) }
 
     Column {
-        Tabs(tabs = tabs, pagerState = pagerState)
+        Tabs(
+            tabs = tabs,
+            pagerState = pagerState,
+            onTabClick = { index ->
+                if (
+                    viewModel.sumScore.value > 0 ||
+                    viewModel.readingScore.value > 0 ||
+                    viewModel.listeningScore.value > 0 ||
+                    viewModel.writingScore.value > 0 ||
+                    viewModel.speakingScore.value  > 0 ||
+                    viewModel.memoText.value.isNotEmpty()
+                    )
+                {
+                    showDialog = true
+                    pendingPage = index
+                } else {
+                    scope.launch { pagerState.animateScrollToPage(index) }
+                }
+            }
+        )
         when (pagerState.currentPage) {
             0 -> IeltsRecordScreen(viewModel = viewModel)
             1 -> ToeflIbtRecordScreen(viewModel = viewModel)
@@ -40,6 +67,28 @@ fun ExamRecordScreen(viewModel: EnglishInfoViewModel) {
         }
         TabsContent(tabs = tabs, pagerState = pagerState)
     }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text("データ喪失の可能性") },
+            text = { Text("入力途中で画面遷移するとデータが喪失しますが宜しいでしょうか？") },
+            confirmButton = {
+                TextButton(onClick = {
+                    viewModel.setMemoText("") // データをクリア
+                    scope.launch { pagerState.animateScrollToPage(pendingPage) }
+                    showDialog = false
+                }) {
+                    Text("はい")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("いいえ")
+                }
+            }
+        )
+    }
 }
 
 @OptIn(ExperimentalPagerApi::class)
@@ -47,9 +96,9 @@ fun ExamRecordScreen(viewModel: EnglishInfoViewModel) {
 @Composable
 private fun Tabs(
     tabs: List<TabItem>,
-    pagerState: com.google.accompanist.pager.PagerState
+    pagerState: com.google.accompanist.pager.PagerState,
+    onTabClick: (Int) -> Unit
 ) {
-    val scope = rememberCoroutineScope()
     TabRow(
         selectedTabIndex = pagerState.currentPage,
         backgroundColor = Color(0xFF6A1B9A),
@@ -65,11 +114,7 @@ private fun Tabs(
                 icon = { /* アイコンを表示しない */ },
                 text = { Text(text = tab.title, color = Color.White) },
                 selected = pagerState.currentPage == index,
-                onClick = {
-                    scope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
-                },
+                onClick = { onTabClick(index) }
             )
         }
     }
