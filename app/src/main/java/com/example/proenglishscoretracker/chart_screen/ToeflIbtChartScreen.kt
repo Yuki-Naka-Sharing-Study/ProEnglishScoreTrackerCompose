ce81efedd6af8eca7d6db73285e78e8107a63f4c
仲優樹 <yu-ki216m@outlook.jp>
2025-02-15 09:41:20 +0900

    “The ${scoreDifference} points have gone up since the last time.” color to Blue in ToeflIbtChartScreen.

--- app/src/main/java/com/example/proenglishscoretracker/chart_screen/ToeflIbtChartScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/chart_screen/ToeflIbtChartScreen.kt
@@ -665,7 +665,7 @@ private fun ComparePreviousScore(
         scoreDifference > 0 -> {
             Triple(
                 Icons.Default.KeyboardArrowUp,
-                Color.Green,
+                Color.Blue,
                 "前回より${scoreDifference}点上がっています。"
             )
         }
