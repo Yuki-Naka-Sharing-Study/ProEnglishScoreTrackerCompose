1f54143eb692066957490cf65e0d6be01d8cd820
仲優樹 <yu-ki216m@outlook.jp>
2025-02-15 09:37:58 +0900

    “The ${scoreDifference} points have gone up since the last time.” color to Blue in EikenChartScreen.

--- app/src/main/java/com/example/proenglishscoretracker/chart_screen/EikenChartScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/chart_screen/EikenChartScreen.kt
@@ -774,7 +774,7 @@ private fun ComparePreviousScore(
         scoreDifference > 0 -> {
             Triple(
                 Icons.Default.KeyboardArrowUp,
-                Color.Green,
+                Color.Blue,
                 "前回より${scoreDifference}点上がっています。"
             )
         }
