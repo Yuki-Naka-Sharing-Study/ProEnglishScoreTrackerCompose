b7792d9da31be7b72549e5bf6ff1a08451a1f157
仲優樹 <yu-ki216m@outlook.jp>
2025-02-24 17:18:57 +0900

    Added “.verticalScroll(rememberScrollState())” in EikenRecordScreen.

--- app/src/main/java/com/example/proenglishscoretracker/record_screen/EikenRecordScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/record_screen/EikenRecordScreen.kt
@@ -21,8 +21,10 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Switch
@@ -89,6 +91,7 @@ fun EikenRecordScreen(viewModel: EnglishInfoViewModel) {
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .clickable(interactionSource = interactionSource, indication = null) {
                 focusManager.clearFocus()
             }
