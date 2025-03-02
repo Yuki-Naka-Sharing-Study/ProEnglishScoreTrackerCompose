09e9938a532f2c5493e6dba17dd84679c20be3ec
仲優樹 <yu-ki216m@outlook.jp>
2025-03-02 12:17:38 +0900

    Add ".verticalScroll(rememberScrollState())" in ToeicDetailScreen.

--- app/src/main/java/com/example/proenglishscoretracker/detail_screen/ToeicDetailScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/detail_screen/ToeicDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.proenglishscoretracker.detail_screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -42,12 +45,41 @@ fun ToeicDetailScreen(
     LaunchedEffect(toeicId, viewModel) {
         viewModel.loadToeicInfoById(toeicId)
     }
-
     val toeicInfo by viewModel.selectedToeicInfo.collectAsState()
     var showAlertDialog by rememberSaveable { mutableStateOf(false) }
 
+    // 削除確認ダイアログ
+    if (showAlertDialog) {
+        AlertDialog(
+            onDismissRequest = { showAlertDialog = false },
+            title = { Text(text = "削除の確認") },
+            text = { Text(text = "このTOEIC データを削除しますか？") },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        viewModel.deleteToeicInfo(toeicId)
+                        showAlertDialog = false
+                        navController.popBackStack()
+                    }
+                ) {
+                    Text(
+                        text = "削除",
+                        color = Color.Red
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showAlertDialog = false }) {
+                    Text(text = "キャンセル")
+                }
+            }
+        )
+    }
+
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         IconButton(
             onClick = { navController.popBackStack() },
@@ -92,35 +124,9 @@ fun ToeicDetailScreen(
             }
         }
 
-        // 削除確認ダイアログ
-        if (showAlertDialog) {
-            AlertDialog(
-                onDismissRequest = { showAlertDialog = false },
-                title = { Text(text = "削除の確認") },
-                text = { Text(text = "このTOEIC データを削除しますか？") },
-                confirmButton = {
-                    TextButton(
-                        onClick = {
-                            viewModel.deleteToeicInfo(toeicId)
-                            showAlertDialog = false
-                            navController.popBackStack()
-                        }
-                    ) {
-                        Text(
-                            text = "削除",
-                            color = Color.Red
-                        )
-                    }
-                },
-                dismissButton = {
-                    TextButton(onClick = { showAlertDialog = false }) {
-                        Text(text = "キャンセル")
-                    }
-                }
-            )
-        }
-
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
             Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.space_120_dp)))
             if (toeicInfo != null) {
                 Row(
