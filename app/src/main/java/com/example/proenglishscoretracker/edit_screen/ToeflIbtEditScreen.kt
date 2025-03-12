731aed287fe60aae675531b9cb6e37a64a9f2eff
仲優樹 <yu-ki216m@outlook.jp>
2025-03-13 08:02:37 +0900

    Rename variable “errorMessage” to “dateErrorMessage” in ToeflIbtEditScreen.

--- app/src/main/java/com/example/proenglishscoretracker/edit_screen/ToeflIbtEditScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/edit_screen/ToeflIbtEditScreen.kt
@@ -47,7 +47,7 @@ fun ToeflIbtEditScreen(
     var memo by remember { mutableStateOf(toeflIbtInfo.memo) }
 
     // 日付の有効性とエラーメッセージの管理
-    var errorMessage by remember { mutableStateOf<String?>(null) } // エラーメッセージを保持
+    var dateErrorMessage by remember { mutableStateOf<String?>(null) } // エラーメッセージを保持
     var isDateValid by remember { mutableStateOf(true) } // 日付の有効性を保持
     var isFocused by remember { mutableStateOf(true) } // フォーカスの有無を管理
 
@@ -68,13 +68,13 @@ fun ToeflIbtEditScreen(
         date = newDate
         // 日付が有効かチェック
         isDateValid = isValidDate(newDate)
-        errorMessage = if (isDateValid) null else "無効な日付です。"
+        dateErrorMessage = if (isDateValid) null else "無効な日付です。"
     }
 
     // フォーカスが外れたときの処理
     fun onFocusChange(focused: Boolean) {
         if (!focused && !isDateValid) {
-            errorMessage = "無効な日付です。"
+            dateErrorMessage = "無効な日付です。"
         }
         isFocused = focused
     }
@@ -138,7 +138,7 @@ fun ToeflIbtEditScreen(
             // 日付のエラーメッセージ
             if (!isDateValid) {
                 Text(
-                    text = errorMessage ?: "",
+                    text = dateErrorMessage ?: "",
                     color = Color.Red,
                     style = MaterialTheme.typography.body2,
                     modifier = Modifier.padding(start = 16.dp)
