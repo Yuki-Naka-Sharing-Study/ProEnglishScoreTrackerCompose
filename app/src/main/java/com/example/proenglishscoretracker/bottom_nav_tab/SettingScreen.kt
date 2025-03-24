0e900651a66fbfab9a7aebd06625e0da810efa2d
仲優樹 <yu-ki216m@outlook.jp>
2025-03-25 08:14:27 +0900

    Corrected “Set Expiration Countdown” to “Qualification Expiration Notification Settings” in the SettingsSettingsScreen. Corrected “Set Exam Date Countdown” to “Set Exam Date Countdown”.

--- app/src/main/java/com/example/proenglishscoretracker/bottom_nav_tab/SettingScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/bottom_nav_tab/SettingScreen.kt
@@ -66,8 +66,8 @@ fun SettingsScreen(navHostController: NavHostController) {
                 context.startActivity(Intent.createChooser(shareIntent, "アプリを共有"))
             },
             "バージョン 1.0.0" to {},
-            "受験日カウントダウンを設定" to { navHostController.navigate("examDayCountdownScreen") },
-            "有効期限カウントダウンを設定" to { navHostController.navigate("expirationSettingsScreen") },
+            "受験日カウントダウン設定" to { navHostController.navigate("examDayCountdownScreen") },
+            "資格有効期限通知設定" to { navHostController.navigate("expirationSettingsScreen") },
             "登録推奨YouTuber" to { navHostController.navigate("youtuberScreen") },
         )
 
