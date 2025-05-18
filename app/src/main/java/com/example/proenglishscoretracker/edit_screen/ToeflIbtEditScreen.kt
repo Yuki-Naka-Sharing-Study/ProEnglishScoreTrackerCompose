4bf790b8ecc22edef850c880fb8b1475fbfff8b6
仲優樹 <yu-ki216m@outlook.jp>
2025-05-18 12:08:59 +0900

    Change the color of ToeflIbtEditScreen to purple.

--- app/src/main/java/com/example/proenglishscoretracker/edit_screen/ToeflIbtEditScreen.kt
+++ app/src/main/java/com/example/proenglishscoretracker/edit_screen/ToeflIbtEditScreen.kt
@@ -202,7 +202,7 @@ fun ToeflIbtEditScreen(
                     }
                 },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Color(0xFF6200EE)
+                    containerColor = Color(0xFF9C27B0)
                 )
             )
         }
